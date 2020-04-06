d845cfe7134ed33380230c65678647caf9eb116f
Emmanuel CARRE <emmanuelCarre@users.noreply.github.com>
2020-04-06 21:47:13 +0200

    Add function to switch Kubernetes context
    To avoid misstake command on wrong Kubernetes cluster, I choose to split
    kubectl config. Each configuration file have to be named as `config-*`
    and to be put into `~/.kube` folder.

--- zsh/custom/kubernetes.zsh
+++ zsh/custom/kubernetes.zsh
@@ -1,3 +1,8 @@
 alias k=kubectl
 alias kns=kubens
-alias kctx=kubectx
+#alias kctx=kubectx
+
+kctx () {
+  local RESULT=$(find ~/.kube/ -maxdepth 1  -type f -name 'config-*' -exec basename {} \; | sort -h | fzf --height 20 --reverse)
+  export KUBECONFIG=~/.kube/${RESULT}
+}
