18667ddedc9dbf805a00bccecee95502db6ba3a1
Emmanuel CARRE <emmanuelCarre@users.noreply.github.com>
2020-04-06 21:41:20 +0200

    Remove zsh plugin kube-ps1

--- zsh/custom/00-zsh.zsh
+++ zsh/custom/00-zsh.zsh
@@ -1,7 +1,7 @@
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
-plugins=(asdf git git-prompt vagrant docker minikube kube-ps1)
+plugins=(asdf git git-prompt vagrant docker minikube)
 
 if [ -d $HOME/.local/bin ]; then
   export PATH=$HOME/.local/bin:$PATH
